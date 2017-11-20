6db55362ced0e7242b670a5acef2cd5de8a179d7
Peter M. Kielar <peter.kielar@tum.de>
2017-11-20 14:57:35 +0100

    Logger in loop bug fixed, multiple log statements

--- momentum-infrastructure/src/main/java/tum/cms/sim/momentum/infrastructure/logging/LoggingManager.java
+++ momentum-infrastructure/src/main/java/tum/cms/sim/momentum/infrastructure/logging/LoggingManager.java
@@ -74,6 +74,7 @@ import tum.cms.sim.momentum.utility.generic.IUnique;
  */
 public class LoggingManager {
 
+	private static boolean loggerInitalized = false;
 	private static ArrayList<Logger> loggers = new ArrayList<>();
 	private LoggingManager() {}
 	/**
@@ -86,6 +87,12 @@ public class LoggingManager {
 			throws BadConfigurationException {
 	
 		LoggingManager.logDebug(LoggerStrings.LogEntry);
+		
+		if(loggerInitalized) {
+			
+			LoggingManager.logDebug(LoggerStrings.LogExists);
+		}
+		
 		boolean doesConsoleLoggerExitsts = false;
 	    int nameIter = 0;
 	    
@@ -147,6 +154,8 @@ public class LoggingManager {
 
 			}
 	    }
+	    
+	    loggerInitalized = true;
 	}
 
 	/**
